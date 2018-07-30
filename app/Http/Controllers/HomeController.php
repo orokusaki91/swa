fbcb840c65f0368bc899b6b2d206045f5d6566dc
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2018-07-30 09:13:46 +0200

    fix home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\PageContent;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-    	$home = App\PageContent::whereHas('page', function ($q) {
+    	$home = PageContent::whereHas('page', function ($q) {
         	$q->where('slug', 'home');
     	})->first();
     	return view('index', compact('home'));

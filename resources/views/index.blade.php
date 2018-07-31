4f12be63abaef414c9bf88aed4ad99d9df79ec53
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2018-07-31 18:55:32 +0200

    fix home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
 		<div class="row">
 			<div class="col-md-4">
 				<div>{!! $home->header_text !!}</div>
-				<div id="collapse-p" class="collapse"> {!! $home->text !!}</div>
+				<div id="collapse-p" class="collapse"> {!! $home->text !!}</div> 
 				<button class="btn rounded-0 mt-4 mb-5" id="btn-seeAll" data-toggle="collapse" href="#collapse-p" role="button" aria-expanded="false" aria-controls="collapse-p">Alles sehen</button>
 			</div>
 			<div class="col-md-8"></div>
